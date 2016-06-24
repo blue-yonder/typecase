ae301211218cac41f62753c43909c3d2488937af
Holger Peters <email@holger-peters.de>
2016-06-24 10:18:35 +0200

    Rename kwargs -> __mappings or __type_mappings

--- typecase/_record.py
+++ typecase/_record.py
@@ -3,8 +3,8 @@ from ._types import ProductType, _This
 
 
 class Record(ProductType):
-    def __init__(self, **kwargs):
-        self.kwargs = kwargs
+    def __init__(self, **type_mappings):
+        self.__type_mappings = type_mappings
 
     def __argtypes(self, parent):
         def _type_representation_for_typechecker(k, typval):
@@ -15,14 +15,14 @@ class Record(ProductType):
             else:
                 raise TypeError("Unexpected type supplied: {}".format(typval))
 
-        type_lst = [_type_representation_for_typechecker(k, self.kwargs[k])
-                    for k in self.kwargs]
+        type_lst = [_type_representation_for_typechecker(k, self.__type_mappings[k])
+                    for k in self.__type_mappings]
         return "{" + ",".join(type_lst) + "}"
 
     def _make_variant_class(self, parent, typename):
         class_definition = _record_template.format(parent=parent.__name__,
                                                    typename=typename,
-                                                   argtypes=self.__argtypes(parent))
+                                                   type_mappings=self.__argtypes(parent))
         namespace = dict(parent=parent,
                          on_same_keys=on_same_keys,
                          __name__='meta_{}'.format(typename))
@@ -40,21 +40,21 @@ def on_same_keys(d1, d2):
 
 _record_template = """
 class {typename}(parent):
-    def __init__(self, **kwargs):
-        self.kwargs = kwargs
-        self._check_types(kwargs)
+    def __init__(self, **mappings):
+        self.__mappings = mappings
+        self._check_types(mappings)
 
     @staticmethod
-    def _check_types(kwargs):
-        argtypes = {argtypes}
+    def _check_types(mappings):
+        type_mappings = {type_mappings}
         format_args = dict()
-        format_args["typenames_def"] = argtypes.keys()
-        format_args["typenames_constr"] = kwargs.keys()
-        if set(kwargs.keys()) != set(argtypes.keys()):
+        format_args["typenames_def"] = type_mappings.keys()
+        format_args["typenames_constr"] = mappings.keys()
+        if set(mappings.keys()) != set(type_mappings.keys()):
             raise TypeError("Attribute names {{typenames_constr}}"
                             "differ from those given in the definition "
                             " {{typenames_def}}".format(**format_args))
-        for item, typ in on_same_keys(kwargs, argtypes):
+        for item, typ in on_same_keys(mappings, type_mappings):
             if isinstance(typ, str):
                 pass
             elif not isinstance(item, typ):
@@ -65,18 +65,18 @@ class {typename}(parent):
                                         itemtype=type(item).__name__))
 
     def __getitem__(self, rhs):
-        return self.kwargs[rhs]
+        return self.__mappings[rhs]
 
     def __getattr__(self, item):
-        if item in self.kwargs:
-            return self.kwargs[item]
+        if item in self.__mappings:
+            return self.__mappings[item]
         else:
             raise AttributeError(item)
 
     def __setattr__(self, item, value):
-        if item == "kwargs":
+        if item == "_{typename}__mappings":
             super({typename}, self).__setattr__(item, value)
-        elif item in self.kwargs:
+        elif item in self.__mappings:
             raise TypeError("TypeError: 'Record' object does not support item assignment")
         else:
             raise AttributeError(item)
@@ -84,7 +84,7 @@ class {typename}(parent):
 
     def __repr__(self):
         args = ", ".join(["{{}}={{}}".format(k, repr(v))
-                          for k, v in self.kwargs.items()])
+                          for k, v in self.__mappings.items()])
         return "{parent}.{typename}({{args}})".format(args=args)
 """
 
